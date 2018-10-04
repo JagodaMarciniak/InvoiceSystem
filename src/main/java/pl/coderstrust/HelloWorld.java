012fa6300583c4571abb7b87a9275a8d754ff4f0
slawek-ja <p.slawek90@gmail.com>
2018-10-04 17:19:15 +0200

    Jacoco plugin

--- src/main/java/pl/coderstrust/HelloWorld.java
+++ src/main/java/pl/coderstrust/HelloWorld.java
@@ -1,7 +1,7 @@
 package pl.coderstrust;
 
 public class HelloWorld {
-    public void displayHelloWorld(){
-        System.out.println("Hello World");
+    public String returnHelloWorldString(){
+        return "Hello World";
     }
 }
