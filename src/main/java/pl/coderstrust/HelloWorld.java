1da1ad9a8653293dbed86be2b10280376d5c3c50
arek-m <arek-m@users.noreply.github.com>
2018-10-08 01:39:55 +0200

    1)Checkstyle Plugin final configuration for a)maven, b)IntelliJ ,2) mvn site plugin added for raport generation, 3)pom.xml corrected for failing etc, 4)google_style_settings.xml corrected after Pawel help

--- src/main/java/pl/coderstrust/HelloWorld.java
+++ src/main/java/pl/coderstrust/HelloWorld.java
@@ -2,7 +2,7 @@ package pl.coderstrust;
 
 public class HelloWorld {
 
-    public String returnHelloWorldString() {
-        return "Hello World";
-    }
+  public String returnHelloWorldString() {
+    return "Hello World";
+  }
 }
--- src/test/java/pl/coderstrust/SampleTest.java
+++ src/test/java/pl/coderstrust/SampleTest.java
@@ -1,21 +1,21 @@
 package pl.coderstrust;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SampleTest {
 
-    @Test
-    public void sampleTest() {
-        //given
-        HelloWorld helloWorld = new HelloWorld();
-        String expected = "Hello World";
+  @Test
+  public void sampleTest() {
+    //given
+    HelloWorld helloWorld = new HelloWorld();
+    String expected = "Hello World";
 
-        //when
-        String result = helloWorld.returnHelloWorldString();
+    //when
+    String result = helloWorld.returnHelloWorldString();
 
-        //then
-        assertEquals(expected, result);
-    }
+    //then
+    assertEquals(expected, result);
+  }
 }
