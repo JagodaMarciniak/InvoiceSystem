51b4b4a6b76a5109f01fe845706f502b79ebe352
slawek-ja <p.slawek90@gmail.com>
2018-10-09 18:13:02 +0200

    Update

---
+++ src/main/java/pl/coderstrust/Company.java
@@ -0,0 +1,13 @@
+package pl.coderstrust;
+
+public class Company {
+    private String name;
+    private String taxIdentificationNumber;
+    private String streetNameAndHouseNumber; //?
+    private String postalCode; //?
+    private String city; //?
+    private String bankAccountNumber;
+    private String country;
+    private String email;
+    private String phoneNumber;
+}
