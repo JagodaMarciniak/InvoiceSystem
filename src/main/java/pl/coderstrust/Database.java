bfababe91aa34459ee913f092a5507b4bb23bf3a
Jagoda <koduzmiana@gmail.com>
2018-10-27 23:53:21 +0200

    3:reformat code,Database&DatabaseOperationEx

--- src/main/java/pl/coderstrust/Database.java
+++ src/main/java/pl/coderstrust/Database.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 public interface Database {
 
-    Invoice saveInvoice(Invoice invoice) throws DatabaseOperationException;
+  Invoice saveInvoice(Invoice invoice) throws DatabaseOperationException;
 
-    Invoice findOneInvoice(String invoiceId) throws DatabaseOperationException;
+  Invoice findOneInvoice(String invoiceId) throws DatabaseOperationException;
 
-    List<Invoice> findAllInvoices() throws DatabaseOperationException;
+  List<Invoice> findAllInvoices() throws DatabaseOperationException;
 
-    List<Invoice> findAllInvoicesBySellerName(String sellerName) throws DatabaseOperationException;
+  List<Invoice> findAllInvoicesBySellerName(String sellerName) throws DatabaseOperationException;
 
-    List<Invoice> findAllInvoicesByBuyerName(String buyerName) throws DatabaseOperationException;
+  List<Invoice> findAllInvoicesByBuyerName(String buyerName) throws DatabaseOperationException;
 
-    Long countInvoices() throws DatabaseOperationException;
+  Long countInvoices() throws DatabaseOperationException;
 
-    void deleteInvoice(Invoice invoice) throws DatabaseOperationException;
+  void deleteInvoice(Invoice invoice) throws DatabaseOperationException;
 
-    boolean invoiceExists(String invoiceId) throws DatabaseOperationException;
+  boolean invoiceExists(String invoiceId) throws DatabaseOperationException;
 }
