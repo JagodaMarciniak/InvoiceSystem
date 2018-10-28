817d61080d7f8b6130fa018db49416440258fbf1
Tomek Szkopinski <tszkopinski@gmail.com>
2018-10-28 13:53:02 +0100

    5 / Invoice class - moves test and generator class variables to methods - revises InvoiceEntriesGenerator class

--- src/test/java/pl/coderstrust/CompanyGenerator.java
+++ src/test/java/pl/coderstrust/CompanyGenerator.java
@@ -1,15 +1,15 @@
 package pl.coderstrust;
 
 public final class CompanyGenerator {
-  private static final String NAME = "Oracle Sp. z o. o.";
-  private static final String TAX_ID = "12345678990";
-  private static final AccountNumber ACCOUNT_NUMBER = new AccountNumber(
-      "PL83620519463926400000847295");
-  private static final ContactDetails CONTACT_DETAILS = new ContactDetails("contact@oracle.com",
-      "+1234567890", "www.oracle.com", new Address("Wyroczni", "13A", "34-760", "Gdynia", "Polska"
-  ));
 
   public static Company getSampleCompany() {
-    return new Company(NAME, TAX_ID, ACCOUNT_NUMBER, CONTACT_DETAILS);
+    final String name = "Oracle Sp. z o. o.";
+    final String taxId = "12345678990";
+    final AccountNumber accountNumber = new AccountNumber(
+        "PL83620519463926400000847295");
+    final ContactDetails contactDetails = new ContactDetails("contact@oracle.com",
+        "+1234567890", "www.oracle.com", new Address("Wyroczni", "13A", "34-760", "Gdynia", "Polska"
+    ));
+    return new Company(name, taxId, accountNumber, contactDetails);
   }
 }
--- src/test/java/pl/coderstrust/InvoiceEntriesGenerator.java
+++ src/test/java/pl/coderstrust/InvoiceEntriesGenerator.java
@@ -1,5 +1,6 @@
 package pl.coderstrust;
 
+import static pl.coderstrust.UnitType.HOUR;
 import static pl.coderstrust.UnitType.PIECE;
 import static pl.coderstrust.Vat.VAT_23;
 
@@ -10,11 +11,11 @@ import java.util.List;
 public final class InvoiceEntriesGenerator {
   public static List<InvoiceEntry> getSampleInvoiceEntries() {
     List<InvoiceEntry> entries = new ArrayList<>();
-    entries.add(new InvoiceEntry("Flashlight BX24", 1L, PIECE,
-        new BigDecimal(100), VAT_23, new BigDecimal(100), new BigDecimal(123)));
+    entries.add(new InvoiceEntry("Flashlight BX24", 2L, PIECE,
+        new BigDecimal(50), VAT_23, new BigDecimal(100), new BigDecimal(123)));
     entries.add(new InvoiceEntry("Swiss Army Knife", 1L, PIECE,
-        new BigDecimal(100), VAT_23, new BigDecimal(100), new BigDecimal(123)));
-    entries.add(new InvoiceEntry("Computer mouse", 1L, PIECE,
+        new BigDecimal(200), VAT_23, new BigDecimal(200), new BigDecimal(246)));
+    entries.add(new InvoiceEntry("Engine oil change", 1L, HOUR,
         new BigDecimal(100), VAT_23, new BigDecimal(100), new BigDecimal(123)));
     return entries;
   }
