7bc673ce7d09e0a31721cf84464e3e41fe469260
slawek-ja <p.slawek90@gmail.com>
2019-01-17 05:17:20 +0100

    31 - Added public package with content in resources

---
+++ src/main/resources/public/invoices.js
@@ -0,0 +1,162 @@
+var app = angular.module('Invoices', []);
+
+app.controller('FindAll', function ($scope, $http) {
+    $http.get('http://127.0.0.1:8080/invoices').
+        then(function (response) {
+            $scope.InvoiceDatabase = response.data;
+            $scope.empty = $scope.InvoiceDatabase.length;
+        });
+});
+
+app.controller('FindById', function ($scope, $http) {
+    $scope.submit = function () {
+        $scope.formData = {};
+        if ($scope.invoice_id) {
+            $http.get(returnValidAddres($scope.invoice_id))
+                .then(
+                    function (response) {
+                        $scope.InvoiceDatabase = response.data;
+                        $scope.formData.answer = 200;
+                        console.log(response.data.message);
+                    },
+                    function (response) {
+                        $scope.formData.answer = 400;
+                        console.log(response);
+                    })
+                .catch(function onError(response) {
+                    console.log(response);
+                });
+        }
+    }
+});
+
+app.controller('AddOrUpdate', function ($scope, $http) {
+    $scope.submit = function () {
+        if ($scope.content) {
+            $scope.formData = {};
+            $http.post('http://127.0.0.1:8080/invoices', $scope.content)
+                .then(
+                    function (response) {
+                        $scope.formData.answer = 200;
+                        console.log(response.data.message);
+                    },
+                    function (response) {
+                        $scope.formData.answer = 400;
+                        console.log(response);
+                    })
+                .catch(function onError(response) {
+                    console.log(response);
+                });
+        }
+    }
+    $scope.getJson = function () {
+        $scope.content = returnValidJSON();
+    }
+});
+
+app.controller('RemoveById', function ($scope, $http) {
+    $scope.submit = function () {
+        if ($scope.invoice_id) {
+            $scope.formData = {};
+            $http.delete(returnValidAddres($scope.invoice_id))
+                .then(
+                    function (response) {
+                        $scope.formData.answer = 200;
+                        console.log(response.data.message);
+                    },
+                    function (response) {
+                        $scope.formData.answer = 400;
+                        console.log(response);
+                    })
+                .catch(function onError(response) {
+                    console.log(response);
+                });
+        }
+    }
+});
+
+function returnValidAddres(invoiceId) {
+    return 'http://127.0.0.1:8080/invoices/' + invoiceId;
+}
+
+function returnValidJSON() {
+    var jsonToString = {
+        "id": 1,
+        "type": "STANDARD",
+        "issueDate": "2018-12-04",
+        "dueDate": "2019-01-01",
+        "seller": {
+            "name": "sampleSeller2",
+            "taxIdentificationNumber": "12345678990",
+            "accountNumber": {
+                "ibanNumber": "PL83620519463926400000847295",
+                "localNumber": "83620519463926400000847295"
+            },
+            "contactDetails": {
+                "email": "contact@oracle.com",
+                "phoneNumber": "+1234567890",
+                "website": "www.oracle.com",
+                "address": {
+                    "street": "Wyroczni",
+                    "number": "13A",
+                    "postalCode": "34-760",
+                    "city": "Gdynia",
+                    "country": "Polska"
+                }
+            }
+        },
+        "buyer": {
+            "name": "sampleBuyer3",
+            "taxIdentificationNumber": "12345678990",
+            "accountNumber": {
+                "ibanNumber": "PL83620519463926400000847295",
+                "localNumber": "83620519463926400000847295"
+            },
+            "contactDetails": {
+                "email": "contact@oracle.com",
+                "phoneNumber": "+1234567890",
+                "website": "www.oracle.com",
+                "address": {
+                    "street": "Wyroczni",
+                    "number": "13A",
+                    "postalCode": "34-760",
+                    "city": "Gdynia",
+                    "country": "Polska"
+                }
+            }
+        },
+        "entries": [
+            {
+                "item": "Flashlight BX24",
+                "quantity": 2,
+                "unit": "PIECE",
+                "price": 50,
+                "vatRate": "VAT_23",
+                "netValue": 100,
+                "grossValue": 123
+            },
+            {
+                "item": "Swiss Army Knife",
+                "quantity": 1,
+                "unit": "PIECE",
+                "price": 200,
+                "vatRate": "VAT_23",
+                "netValue": 200,
+                "grossValue": 246
+            },
+            {
+                "item": "Engine oil change",
+                "quantity": 1,
+                "unit": "HOUR",
+                "price": 100,
+                "vatRate": "VAT_23",
+                "netValue": 100,
+                "grossValue": 123
+            }
+        ],
+        "totalNetValue": 0,
+        "totalGrossValue": 0,
+        "comments": "Some comments"
+    };
+    return JSON.stringify(jsonToString, null, "\t");
+}
